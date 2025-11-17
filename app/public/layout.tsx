02cc1d422066ad2e9e7555a4ddf74ac14d164e2a
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2025-11-17 14:50:54 +0900

    配置を変更

--- app/public/layout.tsx
+++ app/public/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Header } from "@/components/layout/header";
+import { Footer } from "@/components/layout/footer";
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className="antialiased min-h-screen flex flex-col">
         <Header />
         <main className="flex-1">{children}</main>
+        <Footer />
       </body>
     </html>
   );
