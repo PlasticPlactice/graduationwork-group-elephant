8e7ca5c9886fc399e6826abae09e70023ad81c3b
kuya <k.oda.sys22@morijyobi.ac.jp>
2025-11-19 11:00:11 +0900

    ホーム画面作成途中

--- app/(admin)/admin/home/page.tsx
+++ app/(admin)/admin/home/page.tsx
@@ -1,14 +1,26 @@
 import { Icon } from '@iconify/react';
+import { ColorLens } from '@material-ui/icons';
 
 export default function Page() {
     return (
         <main style={{ backgroundColor: "var(--color-bg)", color: "var(--color-text)" }}>
+
+            <h1 style={{ fontSize: "var(--font-size-h1)" }}
+                className='text-center'>開催中のイベント</h1>
+            <div className='text-center'
+                 style={{border:"1px solid var(--color-main)"}}>
+                <p style={{fontSize:"var(--font-size-h1)"}}
+                   className='font-bold underline'>第1回文庫X</p>
+                <p className='font-extrabold '
+                    style={{ color: "#1E40AF", backgroundColor:"#DBEAFE"}}>二次審査中</p>
+                <p>2024年10月30日～2025年10月30日</p>
+            </div>
             {/* todo:枠の大きさを変える */}
             {/* todo:枠に丸み */}
             {/* todo:枠に影 */}
 
             {/* お知らせ・寄贈管理 */}
-            <div style={{ border: "1px solid var(--color-main)", borderRadius: "var(--control-radius)" }}
+            <div style={{ border: "2px solid var(--color-main)", borderRadius: "var(--control-radius)" }}
                 className='flex w-max p-2 shadow-md'>
                 <div className='w-auto mx-1 flex justify-center items-center'>
                     <Icon icon="mdi:megaphone" width="50" height="50"
@@ -24,7 +36,7 @@ export default function Page() {
             </div>
 
             {/* イベント管理 */}
-            <div style={{ border: "1px solid var(--color-main)", borderRadius: "var(--control-radius)" }}
+            <div style={{ border: "2px solid var(--color-main)", borderRadius: "var(--control-radius)" }}
                 className='flex w-max py-2 pl-2 shadow-md'>
                 <div className='w-auto mx-1 flex justify-center items-center'>
                     <Icon icon="mdi:calendar" width="50" height="50"
@@ -40,7 +52,7 @@ export default function Page() {
             </div>
 
             {/* ユーザー管理 */}
-            <div style={{ border: "1px solid var(--color-main)", borderRadius: "var(--control-radius)" }}
+            <div style={{ border: "2px solid var(--color-main)", borderRadius: "var(--control-radius)" }}
                 className='flex w-max py-2 pl-2 pr-5 shadow-md'>
                 <div className='w-auto mx-1 flex justify-center items-center'>
                     <Icon icon="mdi:people" width="50" height="50"
@@ -56,7 +68,7 @@ export default function Page() {
             </div>
 
             {/* パスワード変更 */}
-            <div style={{ border: "1px solid var(--color-main)", borderRadius: "var(--control-radius)" }}
+            <div style={{ border: "2px solid var(--color-main)", borderRadius: "var(--control-radius)" }}
                 className='flex w-max py-2 pl-2 pr-5 shadow-md'>
                 <div className='w-auto mx-1 flex justify-center items-center'>
                     <Icon icon="material-symbols:key" width="50" height="50"
@@ -70,15 +82,6 @@ export default function Page() {
                     className='mb-1'>パスワードを変更</p>
                 </div>
             </div>
-
-            {/* <Icon icon="mdi:calendar" style={{ backgroundColor: "var(--color-main)", color: "var(--color-bg)" }}/> */}
-                {/* <Icon icon="ic:outline-people" style={{ backgroundColor: "var(--color-main)", color: "var(--color-bg)" }}/> */}
-                {/* <Icon icon="mdi:love" style={{ backgroundColor: "var(--color-main)", color: "var(--color-bg)" }}/> */}
-                {/* <Icon icon="material-symbols:key" style={{ backgroundColor: "var(--color-main)", color: "var(--color-bg)" }}/> */}
-                {/* サイズと色を指定 */}
-                {/* <Icon icon="mdi:user" width="24" height="24" color="#ff4463" /> */}
-                {/* CSSクラスでスタイル */}
-                {/* <Icon icon="mdi:settings" className="text-2xl text-blue-500" /> */}
         </main>
         );
 }
