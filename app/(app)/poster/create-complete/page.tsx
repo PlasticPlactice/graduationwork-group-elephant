dc4cd2e00564ac8b9f511cf7545724603c202889
hatano <y.hatano.sys23@morijyobi.ac.jp>
2025-11-24 14:18:10 +0900

    コピーボタンを押したときに吹き出しが出るように変更

--- app/(app)/poster/create-complete/page.tsx
+++ app/(app)/poster/create-complete/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 import Styles from '@/styles/app/account-create.module.css';
 import Image from 'next/image';
+import React from 'react';
 import { useRef } from 'react';
 
 export default function createCompletePage() {
     const inputRef = useRef<HTMLInputElement>(null);
+    const [showTooltip, setshowTooltip] =  React.useState(false);
 
     const copyToUserId = async () =>  {
         if (inputRef.current) {
             await navigator.clipboard.writeText(inputRef.current.value);
+
+            setshowTooltip(true);
+            setTimeout(() => setshowTooltip(false), 1500);
 
             console.log("コピーしました");
         }
@@ -20,13 +25,18 @@ export default function createCompletePage() {
             <p className={`text-2xl font-bold mt-6 mb-10 text-center ${Styles.text24px} ${Styles.mainColor}`}>アカウント登録が<br/>完了しました！</p>
 
             <div className={`text-center py-6 ${Styles.userIdContainer}`} >
-                <p className={`font-bold ${Styles.text16px} ${Styles.subColor}`}>ユーザーID</p>
-                <div className='flex items-center justify-center gap-4'>
-                    <input id='userId' ref={inputRef} defaultValue={"0000000000"} className={`font-bold ${Styles.userId}`}/>
+                <p className={`font-bold ${Styles.text16px} ${Styles.subColor}`}>あなたのユーザーID</p>
+                <div className='relative inline-block items-center'>
+                    <input id='userId' ref={inputRef} defaultValue={"0000000000"} className={`font-bold align-middle ${Styles.userId}`}/>
                     {/* コピペのボタン */}
-                    <button onClick={copyToUserId} className={`${Styles.copyButton} border rounded-full `}>
+                    <button onClick={copyToUserId} className={`${Styles.copyButton} border rounded-full align-middle`}>
                         <Image src="/app/content-copy.png" width={30} height={30} alt="copy" />
                     </button>
+                    {showTooltip && (
+                        <div className={`absolute top-0 left-42 transform -translate-x-1/2 -translate-y-full mb-2 w-2/3 py-1 text-white text-sm rounded ${Styles.tooltip}`}>
+                            コピーしました！
+                        </div>
+                    )}
                 </div>
             </div>
 
