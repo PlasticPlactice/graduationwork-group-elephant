b83abb286c805ca18f0347529728dd8f55bd5651
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2025-11-25 21:43:17 +0900

    add: 寄付ページの一覧を作成

---
+++ app/(public)/donation/page.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Pagination } from "@/components/ui/pagination";
+import { Item } from "@/components/features/item";
+
+const newsItems = Array.from({ length: 10 }, (_, i) => ({
+  id: i + 1,
+  date: "2025-10-01",
+  title: "○○様よりご寄付をいただきました！",
+  image: "/top/image.png",
+}));
+
+export default function DonationPage() {
+  return (
+    <div className="container mx-auto pt-8 px-4">
+      <div className="mb-8">
+        <Button
+          variant="outline"
+          href="/"
+          className="h-5 shadow-md rounded-lg hover:bg-gray-50 text-xs"
+          style={{
+            width: "fit-content",
+            color: "#FF4463",
+            border: "1px solid #FF4463",
+            backgroundColor: "#FFFFFF",
+          }}
+        >
+          &lt; トップへ
+        </Button>
+      </div>
+      <h1 className="text-2xl font-bold text-center tracking-widest mb-8">
+        みなさまからの寄贈本
+      </h1>
+      <div className="max-w-2xl mx-auto">
+        {newsItems.map((item) => (
+          <Item
+            key={item.id}
+            date={item.date}
+            title={item.title}
+            image={item.image}
+          />
+        ))}
+
+        <Pagination />
+      </div>
+    </div>
+  );
+}
