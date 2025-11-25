7a7562f8644f0e2b48c9bfaeae372b6a19acc4cd
kuya <k.oda.sys22@morijyobi.ac.jp>
2025-11-25 11:19:16 +0900

    ホーム画面のcssを以降

--- app/(admin)/admin/home/page.tsx
+++ app/(admin)/admin/home/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
 import { Icon } from '@iconify/react';
+import './style.css';
 
 export const metadata: Metadata = {
   title: "ホーム",
@@ -8,83 +9,63 @@ export const metadata: Metadata = {
 
 export default function Page() {
     return (
-        <main style={{ backgroundColor: "var(--color-bg)", color: "var(--color-text)" }}>
+        <main className="home-main">
 
             <div className='mt-10'>
-                <h1 style={{ fontSize: "var(--font-size-h1)" ,margin:0}}
-                    className='text-center'>開催中のイベント</h1>
-                <div className='text-center py-2 w-3/5 m-auto'
-                    style={{border:"1px solid var(--color-main)"}}>
-                    <p style={{fontSize:"var(--font-size-h1)"}}
-                    className='font-bold underline'>第1回文庫X</p>
-                    <p className='font-extrabold w-max m-auto px-7 py-0.5 rounded-2xl'
-                        style={{ color: "#1E40AF", backgroundColor:"#DBEAFE"}}>二次審査中</p>
-                    <p style={{fontSize:"--font-size-small"}}>2024年10月30日～2025年10月30日</p>
+                <h1 className='text-center event-title'>開催中のイベント</h1>
+                <div className='text-center py-2 w-3/5 m-auto event-container'>
+                    <p className='font-bold underline event-name'>第1回文庫X</p>
+                    <p className='font-extrabold w-max m-auto px-7 py-0.5 rounded-2xl event-status'>二次審査中</p>
+                    <p className="event-date">2024年10月30日～2025年10月30日</p>
                 </div>
             </div>
 
             <div className='grid grid-cols-2 grid-rows-2 w-max m-auto gap-x-14 gap-y-10 mt-18'>
                 {/* お知らせ・寄贈管理 */}
-                <div style={{ border: "2px solid var(--color-main)", borderRadius: "var(--control-radius)" }}
-                    className='flex w-max p-2 shadow-md'>
+                <div className='flex w-max p-2 shadow-md admin-card'>
                     <div className='w-auto mx-1 flex justify-center items-center'>
                         <Icon icon="mdi:megaphone" width="50" height="50"
-                            style={{ backgroundColor: "var(--color-main)", color: "var(--color-bg)" }}
-                            className='rounded-full' />
+                            className='rounded-full admin-icon' />
                     </div>
                     <div>
-                        <h2 className='text-center mb-1'
-                            style={{ fontSize: "var(--font-size-h1)" }}>お知らせ・寄贈管理</h2>
-                        <p style={{ color: "var(--color-input-bg)", fontSize: "var(--font-size-small)" }}
-                        className='mb-1'>お知らせの一覧・投稿・編集</p>
+                        <h2 className='text-center mb-1 card-title'>お知らせ・寄贈管理</h2>
+                        <p className='mb-1 card-description'>お知らせの一覧・投稿・編集</p>
                     </div>
                 </div>
 
                 {/* イベント管理 */}
-                <div style={{ border: "2px solid var(--color-main)", borderRadius: "var(--control-radius)" }}
-                    className='flex w-max py-2 pl-2 shadow-md'>
+                <div className='flex w-max py-2 pl-2 shadow-md admin-card'>
                     <div className='w-auto mx-1 flex justify-center items-center'>
                         <Icon icon="mdi:calendar" width="50" height="50"
-                            style={{ backgroundColor: "var(--color-main)", color: "var(--color-bg)" }}
-                            className='rounded-full p-1' />
+                            className='rounded-full p-1 admin-icon' />
                     </div>
                     <div className='ml-4 mr-16'>
-                        <h2 className='text-center mb-1'
-                            style={{ fontSize: "var(--font-size-h1)" }}>イベント管理</h2>
-                        <p style={{ color: "var(--color-input-bg)", fontSize: "var(--font-size-small)" }}
-                        className='mb-1'>イベントの作成・編集</p>
+                        <h2 className='text-center mb-1 card-title'>イベント管理</h2>
+                        <p className='mb-1 card-description'>イベントの作成・編集</p>
                     </div>
                 </div>
 
                 {/* ユーザー管理 */}
-                <div style={{ border: "2px solid var(--color-main)", borderRadius: "var(--control-radius)" }}
-                    className='flex w-max py-2 pl-2 pr-5 shadow-md'>
+                <div className='flex w-max py-2 pl-2 pr-5 shadow-md admin-card'>
                     <div className='w-auto mx-1 flex justify-center items-center'>
                         <Icon icon="mdi:people" width="50" height="50"
-                            style={{ backgroundColor: "var(--color-main)", color: "var(--color-bg)" }}
-                            className='rounded-full p-1' />
+                            className='rounded-full p-1 admin-icon' />
                     </div>
                     <div className='ml-4 mr-7'>
-                        <h2 className='text-center mb-1'
-                            style={{ fontSize: "var(--font-size-h1)" }}>ユーザー管理</h2>
-                        <p style={{ color: "var(--color-input-bg)", fontSize: "var(--font-size-small)" }}
-                        className='mb-1'>ユーザー情報閲覧・書評閲覧</p>
+                        <h2 className='text-center mb-1 card-title'>ユーザー管理</h2>
+                        <p className='mb-1 card-description'>ユーザー情報閲覧・書評閲覧</p>
                     </div>
                 </div>
 
                 {/* パスワード変更 */}
-                <div style={{ border: "2px solid var(--color-main)", borderRadius: "var(--control-radius)" }}
-                    className='flex w-max py-2 pl-2 pr-5 shadow-md'>
+                <div className='flex w-max py-2 pl-2 pr-5 shadow-md admin-card'>
                     <div className='w-auto mx-1 flex justify-center items-center'>
                         <Icon icon="material-symbols:key" width="50" height="50"
-                            style={{ backgroundColor: "var(--color-main)", color: "var(--color-bg)" }}
-                            className='rounded-full p-1' />
+                            className='rounded-full p-1 admin-icon' />
                     </div>
                     <div className='ml-4 mr-4'>
-                        <h2 className='text-center mb-1'
-                            style={{ fontSize: "var(--font-size-h1)" }}>パスワード変更</h2>
-                        <p style={{ color: "var(--color-input-bg)", fontSize: "var(--font-size-small)" }}
-                        className='mb-1'>パスワードを変更</p>
+                        <h2 className='text-center mb-1 card-title'>パスワード変更</h2>
+                        <p className='mb-1 card-description'>パスワードを変更</p>
                     </div>
                 </div>
             </div>
