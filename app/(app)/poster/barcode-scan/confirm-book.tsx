b83aee941cfcb68cdfa0d81f783c5fdcf839e55a
hatano <y.hatano.sys23@morijyobi.ac.jp>
2025-11-30 16:40:36 +0900

    add: isbnとは？ヘルプモーダルと書籍確認モーダルの作成

---
+++ app/(app)/poster/barcode-scan/confirm-book.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { createPortal } from "react-dom";
+import { Children, useEffect, useState } from "react";
+
+import Styles from "@/styles/app/modal.module.css"
+
+type ModalProps = {
+    open: boolean;
+    onClose: () => void;
+    children: React.ReactNode;
+}
+
+export default function Modal({open, onClose, children}: ModalProps) {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if(!open || !mounted) return null;
+
+    return createPortal(
+        <div className={`${Styles.modalOverlay}`} onClick={onClose}>
+            <div className={`${Styles.modalContent}`} onClick={(e) => e.stopPropagation()}>
+                {children}
+            </div>
+        </div>
+        , document.body
+    );
+}
