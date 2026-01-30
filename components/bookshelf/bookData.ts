fe8b1faf1eb43804b0a3832d0e3bae644702bbbf
hatano <y.hatano.sys23@morijyobi.ac.jp>
2026-01-30 12:13:50 +0900

    update: DB関数の変更にともなって型を修正

--- components/bookshelf/bookData.ts
+++ components/bookshelf/bookData.ts
@@ -7,3 +7,13 @@ export type Book = {
   patternColor?: string;
   review?: string;
 };
+
+export type Reactions = {
+  id: string;
+  reaction: string;
+  icon_path: string;
+  
+  _count?: {
+    bookReviewReactions: number;
+  }
+}
