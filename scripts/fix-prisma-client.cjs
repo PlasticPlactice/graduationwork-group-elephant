aa6ffb3b8f3b32bbe0651b13b93e985b997167ce
kaji <k.notsukitaira.sys23@morijyobi.ac.jp>
2026-02-02 15:04:36 +0900

    お知らせと寄贈情報データ修復

---
+++ scripts/fix-prisma-client.cjs
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+const target = path.join(
+  __dirname,
+  "..",
+  "node_modules",
+  ".prisma",
+  "client",
+  "default.js",
+);
+
+if (!fs.existsSync(target)) {
+  process.exit(0);
+}
+
+const src = fs.readFileSync(target, "utf8");
+const updated = src.replace(
+  "module.exports = { ...require('#main-entry-point') }",
+  "module.exports = { ...require('./index.js') }",
+);
+
+if (updated !== src) {
+  fs.writeFileSync(target, updated, "utf8");
+}
